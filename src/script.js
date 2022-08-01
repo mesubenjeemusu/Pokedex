8ca065d1b185cc16020627723b2ef438f8ef796b
James Methvin <methvin.james@gmail.com>
2022-07-31 19:05:48 -0700

    Search bar and basic search functionality

--- src/script.js
+++ src/script.js
@@ -1,10 +1,31 @@
+function GetSearchTerm() {
+    return document.getElementById("search_name").value;
+}
+
 function httpGet(theUrl) {
     let xmlHttpReq = new XMLHttpRequest();
     xmlHttpReq.open("GET", theUrl, false); 
     xmlHttpReq.send(null);
     return xmlHttpReq.responseText;
-  }
+}
 
-  let respText = httpGet('https://pokeapi.co/api/v2/pokemon/ditto');
-  console.log(respText);
-  document.getElementById("phdata").innerHTML = respText;
+let pokeData;
+
+function QueryPokeAPI() {
+
+    let searchTerm = GetSearchTerm();
+    let respText = httpGet("https://pokeapi.co/api/v2/pokemon/" + searchTerm);
+
+    pokeData = JSON.parse(respText);
+    console.log('Type: ', typeof pokeData);
+    console.log('Contents: ', pokeData)
+
+    let pkmn_name = pokeData["name"];
+    Object.keys(pokeData).forEach((key) => { console.log(key + ": " + pokeData[key])});
+
+    console.log(respText);
+    document.getElementById("phdata").innerHTML = respText;
+    document.getElementById("pokemon_name").innerHTML = pkmn_name;
+}
+
+
