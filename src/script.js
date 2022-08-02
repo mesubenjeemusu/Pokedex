96a61491a3fff139102542edf541774d8959482b
James Methvin <methvin.james@gmail.com>
2022-08-01 22:54:02 -0700

    Added css for style. Added sprite, abilities, and moves to html. Also centered elements on page.

--- src/script.js
+++ src/script.js
@@ -17,15 +17,40 @@ function QueryPokeAPI() {
     let respText = httpGet("https://pokeapi.co/api/v2/pokemon/" + searchTerm);
 
     pokeData = JSON.parse(respText);
-    console.log('Type: ', typeof pokeData);
-    console.log('Contents: ', pokeData)
+    respText = JSON.stringify(pokeData, null, 2);
+    //console.log('Type: ', typeof pokeData);
+    //console.log('Contents: ', pokeData)
 
+    // Get name
     let pkmn_name = pokeData["name"];
-    Object.keys(pokeData).forEach((key) => { console.log(key + ": " + pokeData[key])});
 
-    console.log(respText);
+    // Get abilities
+    let abilities_string = "";
+    let pkmn_abilities = pokeData["abilities"].forEach((dict) => {
+        abilities_string += dict["ability"]["name"] + "\n";
+    });
+
+    // Get moves
+    let moves_string = "";
+    let pkmn_moves = pokeData["moves"].forEach((dict) => {
+        moves_string += dict["move"]["name"] + "\n";
+    });
+
+    // Get sprite
+    let pkmn_sprite_path_string = pokeData["sprites"]["front_default"];
+    console.log(pkmn_sprite_path_string);
+
+    //Object.keys(pokeData).forEach((key) => { console.log(key + ": " + pokeData[key])});
+
+    //console.log(respText);
     document.getElementById("phdata").innerHTML = respText;
     document.getElementById("pokemon_name").innerHTML = pkmn_name;
+    document.getElementById("pokemon_sprite").setAttribute("src", pkmn_sprite_path_string);
+    document.getElementById("pokemon_sprite").setAttribute("visible", "true");
+    document.getElementById("pokemon_abilities").innerHTML = abilities_string;
+    document.getElementById("pokemon_moves").innerHTML = moves_string;
+
+    document.getElementById("parent_content_div").setAttribute("visible", "true");
 }
 
 
